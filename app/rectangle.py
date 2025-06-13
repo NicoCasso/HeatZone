6eb345e9b1e26da33c49886e5be6dbc3839bcf16
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-06-13 15:48:28 +0200

    back to old detection test (which was correct)

--- app/rectangle.py
+++ app/rectangle.py
@@ -11,29 +11,23 @@ class Rectangle() :
 
     def is_near_from(self, other : Rectangle) -> bool :
         xi1 = max(self.x1, other.x1)
-        xi2 = min(self.x2, other.x2)
-        if xi1 > xi2 :
-           return False
-        
         yi1 = max(self.y1, other.y1)
+        xi2 = min(self.x2, other.x2)
         yi2 = min(self.y2, other.y2)
-        if yi1 > yi2:
-            return False
+
+        if xi1 < xi2 and yi1 < yi2:
+            return True
         
-        return True
+        return False
+
     
-    def is_near_from2(self, other : Zone) -> bool :
-        xi1 = max(self.x1, other.x_left)
-        xi2 = min(self.x2, int(other.x_left + other.width))
-        if xi1 > xi2 :
-           return False
-           
- 
-        yi1 = max(self.y1, other.y_top)
-        yi2 = min(self.y2, int(other.y_top+other.height))
-        if yi1 < yi2:
-            return False
+    def is_near_from2(self, zone : Zone) -> bool :
+        other = Rectangle(
+            zone.x_left, 
+            zone.y_top, 
+            int(zone.x_left+zone.width),
+            int(zone.y_top+zone.height))
         
-        return True
+        return self.is_near_from(other)
 
 
